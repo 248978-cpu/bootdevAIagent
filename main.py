07541a298d5a65a249abeff1f68cb0ddf5a7aaf2
248978-cpu <cm248978@gmail.com>
2026-05-02 12:19:19 +0200

    loopje dabei alles klaar

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import argparse
 from google.genai import types
 from prompts import system_prompt
 from call_function import available_functions, call_function
+import sys
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -19,36 +20,48 @@ def main():
     parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-    response = client.models.generate_content(
-        model = "gemini-2.5-flash", 
-        contents = messages,
-        config=types.GenerateContentConfig(
-            tools=available_functions,
-            system_instruction=system_prompt,
-            temperature=0))
-    if response.usage_metadata == None:
-        raise RuntimeError("no usage_metadata")
-    prompt_tokens = response.usage_metadata.prompt_token_count
-    response_tokens = response.usage_metadata.candidates_token_count
-    
-    if args.verbose:
-        print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {prompt_tokens}")
-        print(f"Response tokens: {response_tokens}")
-    if response.function_calls:
-        function_results = []
-        for function in response.function_calls:
-            print(f"Calling function: {function.name}({function.args})")
-            function_result = call_function(function)
-            if not function_result.parts:
-                raise Exception("something went wrong, no content") 
-            if function_result.parts[0].function_response == None:
-                raise Exception("no function response object")
-            if function_result.parts[0].function_response.response == None:
-                raise Exception("no response")
-            function_results.append(function_result.parts)
-            if args.verbose:
-                print(f"-> {function_result.parts[0].function_response.response}")
+    for x in range(20):
+        x += 1
+        response = client.models.generate_content(
+            model = "gemini-2.5-flash", 
+            contents = messages,
+            config=types.GenerateContentConfig(
+                tools=available_functions,
+                system_instruction=system_prompt,
+                temperature=0))
+        if response.usage_metadata == None:
+            raise RuntimeError("no usage_metadata")
+        if response.candidates:
+            for candidate in response.candidates:
+                messages.append(candidate.content)
+        prompt_tokens = response.usage_metadata.prompt_token_count
+        response_tokens = response.usage_metadata.candidates_token_count
+        
+        if args.verbose:
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {prompt_tokens}")
+            print(f"Response tokens: {response_tokens}")
+        if not response.function_calls:
+            print("Response:")
+            print(response.text)
+            return
+        else:
+            function_results = []
+            for function in response.function_calls:
+                print(f"Calling function: {function.name}({function.args})")
+                function_result = call_function(function)
+                if not function_result.parts:
+                    raise Exception("something went wrong, no content") 
+                if function_result.parts[0].function_response == None:
+                    raise Exception("no function response object")
+                if function_result.parts[0].function_response.response == None:
+                    raise Exception("no response")
+                function_results.extend(function_result.parts)
+                if args.verbose:
+                    print(f"-> {function_result.parts[0].function_response.response}")
+            messages.append(types.Content(role="user", parts=function_results))
+        if x == 20:
+            sys.exit("maximum iterations reached")
 
     
         
